c86a42a1c01371263db3a37d9ba6b63d624f6ffc
Legenhp <zayeedinamdar@gmail.com>
2026-07-10 23:50:58 +0530

    Core Java

--- Sorting/src/com/sort/sorting/InsertionSort.java
+++ Sorting/src/com/sort/sorting/InsertionSort.java
@@ -3,8 +3,32 @@ package com.sort.sorting;
 public class InsertionSort {
 
 	
+	private static int[] inser() {
+		int[] arr = {8,9,9,1,6,1,5,3,2,7};
+		for(int i=1;i<arr.length;i++) {
+			int temp=i;
+			for(int j=i-1;j>=0;j--){
+				
+				if(arr[j]>arr[temp]) {
+					int swap=arr[temp];
+					arr[temp]=arr[j];
+					arr[j]=swap;
+					temp=j;
+				}
+				
+			}
+			
+			}
+		return arr;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int []arr=inser();
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
 
 	}
 
